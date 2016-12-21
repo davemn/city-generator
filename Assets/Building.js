80d2ab003e2213d0959d3e05cd43d83e857bfbeb
David Mann <stratlater@gmail.com>
2016-12-21 17:57:29 -0600

    Begin port of setupBuildings(), Building class.

---
+++ Assets/Building.js
@@ -0,0 +1,120 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+public class Building extends MonoBehaviour
+{
+	public var meshgen: MeshGenerator;
+
+	//map val (0-1) to a range with optional weight (default 1.0)
+	private function MapToRange(val: float, min: float, max: float){
+		return this.MapToRange(val, min, max, 1.0f);
+	}
+
+	//map val (0-1) to a range with optional weight (default 1.0)
+	private function MapToRange(val: float, min: float, max: float, exp: float){
+		var weighted = Mathf.Pow(val, exp);
+		//make the highest little higher
+		if (val >= 0.9f) 
+			weighted = val;
+		var num = Mathf.Floor(weighted * (max - min)) + min;
+		return num;
+	}
+
+	//get a random int in range
+	private function getRandInt(min, max, exp) {
+		return mapToRange(Random.value, min, max, exp);
+	}
+
+	// TODO move getBoxMeshOpts somewhere to be shared with setupBridges
+
+	//create a box mesh with a geometry and material
+	private function getBoxMeshOpts(options: Hashtable) {
+		var o=options;
+		return meshgen.getBoxMesh(o.color, o.w, o.h, o.l, o.x, o.y, o.z, o.shadow);
+	}
+
+	public function Building(opts) {
+		this.parts = new List.<GameObject>();
+		//50% chance of building having a rim.
+		var rim = getRandInt(3,5);
+		var inset = getRandInt(2,4);
+		var rim_opts = {
+			color: opts.color,
+			h: rim,
+			y: opts.h + (rim/2) + city.curb_h,
+			shadow: false
+		};
+		//building core
+		this.parts.Add(getBoxMeshOpts({
+			color: opts.color,
+			w: opts.w,
+			h: opts.h,
+			l: opts.l,
+			x: opts.x,
+			y: (opts.h/2)+city.curb_h,
+			z: opts.z,
+			shadow: true
+		}));
+		//draw rim on top of some buildings
+		if(chance(50)){
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: opts.w,
+				l: inset,
+				x: opts.x,
+				z: opts.z - (opts.l/2 - inset/2)
+			})));
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: opts.w,
+				l: inset,
+				x: opts.x,
+				z: opts.z + (opts.l/2 - inset/2)
+			})));
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: inset,
+				l: opts.l-(inset*2),
+				x: opts.x - (opts.w/2 - inset/2),
+				z: opts.z
+			})));
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: inset,
+				l: opts.l-(inset*2),
+				x: opts.x + (opts.w/2 - inset/2),
+				z: opts.z
+			})));		
+		}
+		//additional details
+		if(chance(50)){
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: getRandInt(opts.w/4, opts.w/2),
+				l: getRandInt(opts.l/4, opts.l/2),
+				x: opts.x - (5*randDir()),
+				z: opts.z - (5*randDir())
+			})));
+		}
+		//antenna only on tall buildings
+		if(chance(25) && opts.tall){
+			this.parts.Add(getBoxMeshOpts(_.assign(rim_opts, {
+				w: 3,
+				l: 3,
+				x: opts.x - (5*randDir()),
+				z: opts.z - (5*randDir()),
+				h: getRandInt(city.build_max_h/5, city.build_max_h/3)
+			})));
+		}
+		if(chance(25) && opts.tall){
+			var top = getBoxMeshOpts(_.assign(rim_opts, {
+				w: opts.w - (opts.w/3),
+				l: opts.w - (opts.w/3),
+				x: opts.x,
+				z: opts.z,
+				h: getRandInt(15, 30)
+			}));
+			top.castShadow = false;
+			this.parts.Add(top);
+		}
+		//merged mesh
+		var merged = meshgen.mergeMeshes(this.parts.ToArray());
+		this.group = merged;
+	}
+};
--- Assets/GenerateBlocks.js
+++ Assets/GenerateBlocks.js
@@ -25,6 +25,67 @@ function MapToRange(val: float, min: float, max: float, exp: float){
 	return num;
 }
 
+//recursively create buildings return array of meshes
+function SetupBuildings(x, z, w, l, h, sub, color){
+	// var street_meshes = new List.<GameObject>();
+	SetupBuildings(x,z,w,l,h,sub,color, new List.<GameObject.();
+}
+
+function SetupBuildings(x, z, w, l, h, sub, color, buildings){
+	var offset, half, between;
+	//array of buildings for this block
+	buildings = buildings || [];
+	var depth = Math.pow(2, city.subdiv);
+	var tall = Math.round((h/city.build_max_h)*100) > 90;
+	var slice_deviation = 15;
+	//really tall buildings take the whole block
+	if(sub<1 || tall){
+		building = new Building({
+			h: getRandInt(h-city.block_h_dev, h+city.block_h_dev),
+			w: w, 
+			l: l,
+			x: x,
+			z: z,
+			tall:tall,
+			color: color
+		});
+		buildings.push(building.group);
+		//add all buildings in this block to scene as a single mesh
+		if(buildings.length >= depth || tall){
+			scene.add(mergeMeshes(buildings));
+		}
+	}
+	else{
+		//recursively slice the block until num of subdivisions met
+		//TODO: simplify this
+		var dir = (w==l) ? chance(50) : w>l;
+		if(dir){
+			offset = Math.abs(getRandInt(0, slice_deviation));
+			between = (city.inner_block_margin/2);
+			half = w/2;
+			var x_prime = x + offset; 
+			var w1 = Math.abs((x+half)-x_prime) - between;
+			var w2 = Math.abs((x-half)-x_prime) - between;
+			var x1 = x_prime + (w1/2) + between;
+			var x2 = x_prime - (w2/2) - between;
+			setupBuildings(x1, z, w1, l, h, sub-1, color, buildings);
+			setupBuildings(x2, z, w2, l, h, sub-1, color, buildings);
+		}
+		else{
+			offset = Math.abs(getRandInt(0, slice_deviation));
+			between = (city.inner_block_margin/2);
+			half = l/2;
+			var z_prime = z + offset; 
+			var l1 = Math.abs((z+half)-z_prime) - between;
+			var l2 = Math.abs((z-half)-z_prime) - between;
+			var z1 = z_prime + (l1/2) + between;
+			var z2 = z_prime - (l2/2) - between;
+			setupBuildings(x, z1, w, l1, h, sub-1, color, buildings);
+			setupBuildings(x, z2, w, l2, h, sub-1, color, buildings);
+		}
+	}
+}
+
 function Start () {
 	this.city = new CityConfig();
 	this.meshgen = GetComponent.<MeshGenerator>();
@@ -52,12 +113,13 @@ function Start () {
 				curb.transform.parent = transform;
 				curb.transform.localPosition = new Vector3(x, city.curb_h/2, z);
 
-				/* TODO
 				//create buildings in debug mode the building color is mapped to the hightmap
 				if(hm > city.tree_threshold) {
-					var building_color = DEBUG ? getGreyscaleColor(hm) : colors.BUILDING;
+					// var building_color = DEBUG ? getGreyscaleColor(hm) : colors.BUILDING;
+					var building_color = MyColors.BUILDING;
 					setupBuildings(x, z, inner, inner,  h, city.subdiv, building_color);
 				}
+				/* TODO
 				//create tree meshes
 				else{ setupPark(x, z, inner, inner); }
 				*/

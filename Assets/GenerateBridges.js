a013ab360a2c7204a7a977ef8affe8ee78955955
David Mann <stratlater@gmail.com>
2017-01-05 14:31:47 -0600

    Bridge gen: replaced heightmap color -> float conv. w/ Lodash.Map()

--- Assets/GenerateBridges.js
+++ Assets/GenerateBridges.js
@@ -10,21 +10,34 @@ private var buildingContainer: Transform;
 private var treeContainer: Transform;
 
 // TODO to be used by _.reject call below
+/*
 private function ValidRowElem(n: float) {
 	return n >= city.tree_threshold;
 }
+*/
+
+private function ColorToFloat(c: Color): float {
+	return c.grayscale;
+}
 
 //get rows or columns with no buildings
-private function GetEmptyRows() {
-	// TODO replace with Lodash.Map() - https://forum.unity3d.com/threads/unityscript-callback-functions-cannot-cast-from-source-type-to-destination-type.199038/
+private function GetEmptyRows(): List.<float> {
+	// <<<
+	/*
 	var rowsColor = new List.<Color>(heightmap.GetPixels());
 	var rows = new List.<float>();
 	for(var p=0; p < rowsColor.Count; p++){
 		rows.Add(rowsColor[p].grayscale);
 	}
+	*/
+	// ---
+	var rowsColor = new List.<Color>(heightmap.GetPixels());
+	var rows = CityGenerator.Lodash.Map(rowsColor, ColorToFloat);
+	return rows;
+	// >>>
 
 	/* - begin ported code - */
-
+	/*
 	var i, low, lri, lci, empty = [];
 	//loop through rows
 	for(i=0; i<rows.Count;i++){
@@ -47,6 +60,7 @@ private function GetEmptyRows() {
 		}
 	}
 	return empty;
+	*/
 }
 
 function Start () {
@@ -56,8 +70,12 @@ function Start () {
 	
 	/* - Ported code - createBridges() */
 	//create bridges
-	var bridges = CityGenerator.Lodash.Shuffle(GetEmptyRows()).RemoveRange(0, city.bridge_max);
+	// bridges = _.shuffle(getEmptyRows()).splice(0, city.bridge_max);	
+	var bridges = CityGenerator.Lodash.Shuffle(GetEmptyRows());
+	bridges.RemoveRange(0, Mathf.FloorToInt(city.bridge_max));
+
 	// TODO continue porting
+	/*
 	var parts = new List.<GameObject>();
 	for(var i=0;i<bridges.length;i++){
 		var lx = getCoordinateFromIndex(bridges[i].index, city.width);
@@ -86,4 +104,5 @@ function Start () {
 		}
 	}
 	if(parts.length) scene.add(mergeMeshes(parts));
+	*/
 }

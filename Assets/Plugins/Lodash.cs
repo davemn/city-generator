4b553eaea784c00e191501c1036863881a8e878b
David Mann <stratlater@gmail.com>
2017-01-05 14:31:06 -0600

    Lodash sig. operate on List<>; Map creates List<> instead of generator.

--- Assets/Plugins/Lodash.cs
+++ Assets/Plugins/Lodash.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace CityGenerator {
 	public static class Lodash {
 		// Fisher-Yates shuffle (same alg. as Lodash)
-		// Mutates the input List
-		public static void Shuffle(IList<T> lst) {
+		// NB. Does not create a new list! Mutates and returns
+		public static List<T> Shuffle<T>(List<T> lst) {
 			// Loops through array
 			for (int i = lst.Count-1; i > 0; i--) {
 				// Randomize a number between 0 and i (so that the range decreases each time)
@@ -16,13 +16,22 @@ namespace CityGenerator {
 				lst[i] = lst[rnd];
 				lst[rnd] = temp;
 			}
+			return lst;
 		}
 
 		// private delegate T MapIteratee<S,T>(S value, int index, IList<S> collection);
 		// public static IList<T> Map<S,T>(IList<S> collection, MapIteratee<> iteratee) {
 
-		public static IList<T> Map<S,T>(IList<S> collection, Func<S,T> iteratee) {
-			return Enumerable.Select<S,T> (collection, iteratee);
+		public static List<T> Map<S,T>(List<S> collection, System.Func<S,T> iteratee) {
+			IEnumerable<T> res = collection.Select<S,T> (iteratee);
+
+			// make Select() synchronous by forcing a single iteration,
+			// and copying the result to a new List<>
+			List<T> resList = new List<T> ();
+			foreach (T elem in res) {
+				resList.Add (elem);
+			}
+			return resList;
 		}
 
 		// TODO _.reject
